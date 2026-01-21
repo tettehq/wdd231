a321c0419f94a807814c36762f0f1b37161ca65a
Aaron Tetteh Quarshie <ronyquash@gmail.com>
2026-01-21 21:59:06 +0000

    chamber complete

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,62 @@
+const url = "/chamber/data/members.json";
+
+const cards = document.querySelector("#cards");
+
+async function getBusinessData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data);
+  displayBusinesses(data.businesses);
+}
+
+getBusinessData();
+
+const displayBusinesses = (businesses) => {
+    businesses.forEach(business => {
+        let card = document.createElement("section");
+        let companyName = document.createElement("h3");
+        let image = document.createElement("img");
+        let address = document.createElement("span");
+        let phone = document.createElement("span");
+        let email = document.createElement("a");
+
+        companyName.textContent = business.companyName;
+        address.textContent = business.address;
+        phone.textContent = business.phone;
+        email.setAttribute("href", business.email);
+        email.textContent = business.email;
+        image.setAttribute(
+          "src",
+          `https://placehold.co/200x100?text=${business.companyName}`,
+        );
+        image.setAttribute(
+          "alt",
+          `Logo of ${business.companyName}`,
+        );
+        image.setAttribute("loading", "lazy");
+
+        card.appendChild(companyName);
+        card.appendChild(image);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(email);
+
+        cards.appendChild(card);
+    });
+}
+
+// display layout buttons
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+
+gridbutton.addEventListener("click", (e) => {
+  cards.classList.remove("list");
+  gridbutton.classList.add("selected");
+  listbutton.classList.remove("selected");
+});
+listbutton.addEventListener("click", () => {
+  cards.classList.add("list");
+  listbutton.classList.add("selected");
+  gridbutton.classList.remove("selected");
+});
+
